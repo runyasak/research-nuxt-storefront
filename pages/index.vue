d4cce13209123d30639ac91392df6eae9a0a3204
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-12-16 18:47:18 +0700

    Implement fetchProducts with mapImageSrc computed

--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,52 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container id="index-page">
+    <div class="text-center">
+      <h1>Get Inspired</h1>
+      <v-row>
+        <v-col offset="2" cols="8">
+          <v-row>
+            <v-col
+              v-for="(url, index) in productImageUrls"
+              :key="index"
+              cols="12"
+              md="6"
+              lg="4">
+              <img
+                :src="mapImageSrc(url)"
+                class="_object-fit-cover"
+                width="100%"
+                height="100%"
+                alt="Product Image">
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
+import { getImage } from '@/resources/image'
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  name: 'IndexPage',
+  data () {
+    return {
+      productImageWidth: 600,
+      productImageHeight: 600
+    }
+  },
+  computed: {
+    productImageUrls () {
+      return this.$store.state.catalog.products.map(product => product.source.smallImage)
+    }
+  },
+  mounted () {
+    this.$store.dispatch('catalog/fetchProducts', { size: 9 })
+  },
+  methods: {
+    mapImageSrc (url) {
+      return getImage(this.productImageWidth, this.productImageHeight, url, 'resize')
+    }
   }
 }
 </script>
