b3299a0da95e71877fe7aa11580aa6502620479d
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-12-16 15:53:08 +0700

    Add rules in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,25 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/component-name-in-template-casing': 'off',
+    'comma-dangle': ['error', 'never'],
+    'semi': ['error', 'never'],
+    'quote-props': ['error', 'consistent-as-needed'],
+    'import/no-unresolved': 'off',
+    'space-before-function-paren': 'off',
+    'object-curly-newline': 'off',
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'acc', // for reduce accumulators
+        'state',
+        'el',
+        'Vue'
+      ]
+    }]
   }
 }
