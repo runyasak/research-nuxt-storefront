ceb7d2628b21b5e1f771af4f3501804a085362b6
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-12-17 17:08:12 +0700

    Add fetchCategories in catalog with implementing axios

--- store/catalog.js
+++ store/catalog.js
@@ -3,19 +3,36 @@ import { camelizeKeys } from 'humps'
 const DEFAULT_CATALOG = 'vue_storefront_catalog'
 
 export const state = () => ({
+  categories: [],
   products: []
 })
 
 export const mutations = {
+  SET_CATEGORIES (state, value) {
+    state.categories = value
+  },
   SET_PRODUCTS (state, value) {
     state.products = value
   }
 }
 
 export const actions = {
+  async fetchCategories ({ commit }, { size = 30, from = 0 }) {
+    const queryString = {
+      _source_exclude: 'sgn',
+      _source_include: 'id,*.children_data.id,*.id,children_count,sku,name,is_active,parent_id,level,url_key,url_path,product_count,path',
+      sort: 'position:asc',
+      request: { query: { bool: { filter: { bool: { must: [{ terms: { level: [2] } }, { terms: { is_active: [true] } }] } } } } },
+      size,
+      from
+    }
+
+    const result = await this.$axios.$get(`/api/catalog/${DEFAULT_CATALOG}/category/_search`, { params: queryString })
+    commit('SET_CATEGORIES', camelizeKeys(result.hits.hits))
+  },
   async fetchProducts ({ commit }, { size = 10, from = 0 }) {
-    const result = await fetch(`http://localhost:8080/api/catalog/${DEFAULT_CATALOG}/product/_search?size=${size}&from=${from}`)
-    const jsonData = await result.json()
-    commit('SET_PRODUCTS', camelizeKeys(jsonData.hits.hits))
+    const params = { size, from }
+    const result = await this.$axios.$get(`/api/catalog/${DEFAULT_CATALOG}/product/_search`, { params })
+    commit('SET_PRODUCTS', camelizeKeys(result.hits.hits))
   }
 }
