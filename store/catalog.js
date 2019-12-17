b0692c1f0a5673e4b078ddd00a126dbd8fdb30d7
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-12-17 15:32:06 +0700

    Implement destructuring on params of fetchProducts

--- store/catalog.js
+++ store/catalog.js
@@ -13,10 +13,7 @@ export const mutations = {
 }
 
 export const actions = {
-  async fetchProducts ({ commit }, params) {
-    const size = params.size || 10
-    const from = params.from || 0
-
+  async fetchProducts ({ commit }, { size = 10, from = 0 }) {
     const result = await fetch(`http://localhost:8080/api/catalog/${DEFAULT_CATALOG}/product/_search?size=${size}&from=${from}`)
     const jsonData = await result.json()
     commit('SET_PRODUCTS', camelizeKeys(jsonData.hits.hits))
