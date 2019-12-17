6590ff6ab66e7bc399442eaa4bf9ed79a2f918c9
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-12-17 17:57:46 +0700

    Add CategoryUrlKey page with categoryName

---
+++ pages/category/_urlKey.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container id="cateogory-urlKey">
+    <h1>{{ categoryName }}</h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'CategoryUrlKey',
+  async asyncData ({ store, params: { urlKey } }) {
+    await store.dispatch('catalog/fetchCategoryByUrlKey', { urlKey })
+
+    return {
+      categoryName: store.state.catalog.category.source.name
+    }
+  }
+}
+</script>
