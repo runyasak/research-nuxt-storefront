0bb7c640c0b679e5fefe6babf27b2b7bd0981884
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2019-12-19 14:54:51 +0700

    Change url to demo on getImage method

--- resources/image.js
+++ resources/image.js
@@ -1,3 +1,3 @@
 export const getImage = (width, height, url, operation = 'resize') => {
-  return `http://localhost:8080/img/${width}/${height}/${operation}${url}`
+  return `https://demo.storefrontcloud.io/img/${width}/${height}/${operation}${url}`
 }
